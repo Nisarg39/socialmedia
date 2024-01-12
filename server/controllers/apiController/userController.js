f2f51c3ed2c31e71fbd992ddef6b72a702242f6d
Nisarg Shah <shah.nisarg39@gmail.com>
2024-01-12 18:38:44 +0530

    stable version

--- server/controllers/apiController/userController.js
+++ server/controllers/apiController/userController.js
@@ -49,10 +49,10 @@ module.exports.signUp = async function(req,res){
 }
 
 module.exports.signin = async function(req, res){
-    console.log(req.body)
+    // console.log(req.body)
     // console.log("user connected")
     const userData = await User.findOne({username: req.body.username, password: req.body.password})
-    console.log(userData)
+   
     if(userData){
         const payload = {
             userid: userData._id
