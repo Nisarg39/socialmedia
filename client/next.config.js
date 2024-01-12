8388dacd968508e2e8b0333306739a88960131d2
Nisarg Shah <shah.nisarg39@gmail.com>
2024-01-12 15:42:08 +0530

    added  in next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -12,7 +12,13 @@ const nextConfig = {
                 // hostname: "192.168.1.215"
                 hostname: "peach-4m9r"
             }
-        ]
+        ],
+        headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+            { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
         
     }
 }
