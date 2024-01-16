895f364463082644686896293985fadcc7eca9ab
Nisarg Shah <shah.nisarg39@gmail.com>
2024-01-16 22:05:28 +0530

    configuring vercel server with render server

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2eb4e2b257a238db"; }
+/******/ 		__webpack_require__.h = function() { return "5d1a9851fe52434a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/next.config.js
+++ client/next.config.js
@@ -4,17 +4,17 @@ const nextConfig = {
         // URL: 'http://localhost:8000', 
         BACKENDURL: "dd",
         // URL: 'http://192.168.1.215:8000'
-        // URL: 'https://peach-4m9r.onrender.com',
-        URL: 'https://b145-2401-4900-1c43-9f9a-38f8-15a7-789c-b8a2.ngrok-free.app'
+        URL: 'https://peach-4m9r.onrender.com',
+        // URL: 'https://b145-2401-4900-1c43-9f9a-38f8-15a7-789c-b8a2.ngrok-free.app'
       },
       images: {
         remotePatterns:[
             {
                 hostname: "localhost",
                 // hostname: "192.168.1.215"
-                // hostname: "peach-4m9r"
+                hostname: "peach-4m9r"
                 // hostname: "https://9855-103-47-153-161.ngrok-free.app"
-                hostname: "b145-2401-4900-1c43-9f9a-38f8-15a7-789c-b8a2.ngrok-free.app"
+                // hostname: "b145-2401-4900-1c43-9f9a-38f8-15a7-789c-b8a2.ngrok-free.app"
             }
         ],   
     },
